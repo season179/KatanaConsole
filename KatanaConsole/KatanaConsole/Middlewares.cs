a9f4e45af4ffdad68fe10581268206452d22c342
Season Saw <season.saw@gmail.com>
2015-06-25 22:16:58 +0800

    Creating Middleware Components as Stand-Alone Classes

---
+++ KatanaConsole/KatanaConsole/Middlewares.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace KatanaConsole
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    class Middlewares
+    {
+    }
+
+    public class MyMiddlewareComponent
+    {
+        AppFunc _next;
+        public MyMiddlewareComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            IOwinContext context = new OwinContext(environment);
+            await context.Response.WriteAsync("<h1>Hello from My First Middleware</h1>");
+            await _next.Invoke(environment);
+        }
+    }
+
+    public class MyOtherMiddlewareComponent
+    {
+        AppFunc _next;
+        public MyOtherMiddlewareComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            IOwinContext context = new OwinContext(environment);
+            await context.Response.WriteAsync("<h1>Hello from My Other Middleware</h1>");
+            await _next.Invoke(environment);
+        }
+    }
+}
--- KatanaConsole/KatanaConsole/Startup.cs
+++ KatanaConsole/KatanaConsole/Startup.cs
@@ -14,33 +14,8 @@ namespace KatanaConsole
     {
         public void Configuration(IAppBuilder app)
         {
-            var middleware = new Func<AppFunc, AppFunc>(MyMiddleware);
-            var otherMiddleware = new Func<AppFunc, AppFunc>(MyOtherMiddleware);
-
-            app.Use(middleware);
-            app.Use(otherMiddleware);
-        }
-
-        public AppFunc MyMiddleware(AppFunc next)
-        {
-            AppFunc appFunc = async (IDictionary<string, object> environment) =>
-                {
-                    IOwinContext context = new OwinContext(environment);
-                    await context.Response.WriteAsync("<h1>Hello from My First Middleware</h1>");
-                    await next.Invoke(environment);
-                };
-            return appFunc;
-        }
-
-        public AppFunc MyOtherMiddleware(AppFunc next)
-        {
-            AppFunc appFunc = async (IDictionary<string, object> environment) =>
-                {
-                    IOwinContext context = new OwinContext(environment);
-                    await context.Response.WriteAsync("<h1>Hello from My Other Middleware</h1>");
-                    await next.Invoke(environment);
-                };
-            return appFunc;
+            app.Use<MyMiddlewareComponent>();
+            app.Use<MyOtherMiddlewareComponent>();
         }
     }
 }
