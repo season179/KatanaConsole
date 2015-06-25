f59566d202c5b9a980bcaccd08beae002bc80c1b
Season Saw <season.saw@gmail.com>
2015-06-25 21:34:14 +0800

    initial commit

---
+++ KatanaConsole/KatanaConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace KatanaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApp.Start<Startup>("http://localhost:8080");
+            Console.WriteLine("Server Started; Press enter to Quit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KatanaConsole/KatanaConsole/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace KatanaConsole
+{
+    using System.IO;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var middleware = new Func<AppFunc, AppFunc>(MyMiddleware);
+            app.Use(middleware);
+        }
+
+        public AppFunc MyMiddleware(AppFunc next)
+        {
+            AppFunc appFunc = async (IDictionary<string, object> environment) =>
+                {
+                    // Do something with the incoming request:
+                    var response = environment["owin.ResponseBody"] as Stream;
+                    using (var writer = new StreamWriter(response))
+                    {
+                        await writer.WriteAsync("<h1>Hello from my first middleware</h1>");
+                    }
+                    // Call the next Middleware in the chain:
+                    await next.Invoke(environment);
+                };
+            return appFunc;
+        }
+    }
+}
